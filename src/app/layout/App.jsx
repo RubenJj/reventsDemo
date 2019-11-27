aa11b34170e87d89dc8e4e58a25cbbd4c482f1bc
RubenJj <38173365+RubenJj@users.noreply.github.com>
2019-11-27 14:01:39 +0100

    Create Event toggle EventForm

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,22 @@
-import React, {Component} from 'react';
-import { Button } from 'semantic-ui-react';
+import React, {Component, Fragment} from 'react';
+import {Container} from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render(){
     return (
-      <div>
-        <h1>Re-vents</h1>
-        
-      <Button icon='smile' content='React button' />
-      <EventDashboard/>
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container className ="main">
+
+          <EventDashboard/>
+
+        </Container>
+      </Fragment>
+      
+      
+      
     );
   }
 }
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,18 +1,86 @@
 import React, { Component } from 'react'
-import { Grid, GridColumn } from 'semantic-ui-react'
+import { Grid, GridColumn, Button } from 'semantic-ui-react'
+import EventList from '../EventList/EventList'
+import EventForm from '../EventForm/EventForm'
+
+const eventsFromDashboard = [
+  {
+    id: '1',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27T11:00:00+00:00',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      }
+    ]
+  },
+  {
+    id: '2',
+    title: 'Trip to Punch and Judy Pub',
+    date: '2018-03-28T14:00:00+00:00',
+    category: 'drinks',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Tom',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+    attendees: [
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      },
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      }
+    ]
+  }
+]
+
 
 class EventDashboard extends Component {
+    state = {
+        events: eventsFromDashboard,
+        isOpen: true
+    };
+
+    handleIsOpenToggle = () => {
+        this.setState((prevState) => ({
+            isOpen: !prevState.isOpen
+        }))
+    };
+
     render() {
+        const {events, isOpen} = this.state;
         return (
             <Grid>
                 <GridColumn width={10}>
-                    <h2>Left Column</h2>
+                    <EventList events={events}/>
                 </GridColumn>
                 <GridColumn width={6}>
-                    <h2>Right Column</h2>
+                    <Button onCLick ={this.handleIsOpenToggle} positive content='Create Event'/>
+                    {isOpen && <EventForm/>}
+                    
                 </GridColumn>
             </Grid>
-        )
+        );
     }
 }
 
